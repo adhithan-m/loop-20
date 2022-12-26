f59272568330b9e40e2cdc33cec92ff397d350c8
adhithan-m <116782079+adhithan-m@users.noreply.github.com>
2022-12-26 19:59:33 +0530

    limit.

---
+++ limit/js/limit.js
@@ -0,0 +1,12 @@
+var x=parseInt(prompt("enter limit :"));
+for(i=0;i<x;i++)
+{
+	for(j=0;j<x;j++)
+	{
+		document.write("*" +"&nbsp &nbsp");
+		//document.write("*");
+		
+	}
+	document.write("<br>");
+}
+
